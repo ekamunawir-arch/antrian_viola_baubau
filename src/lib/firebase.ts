0a2d71ef347432aa0b796cab7620d03b740474a9
Dani And friend <eka.munawir@gmail.com>
2026-03-06 06:59:58 +0000

    PERINTAH UNTUK IDX
    Buat file baru:
    
    src/lib/firebase.ts
    
    Isi dengan:
    
    i

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvOz6w5WnJfAcMDzXfXIK3czlWtKJwrEw",
+  authDomain: "studio-4693874887-c194d.firebaseapp.com",
+  projectId: "studio-4693874887-c194d",
+  storageBucket: "studio-4693874887-c194d.firebasestorage.app",
+  messagingSenderId: "495492585274",
+  appId: "1:495492585274:web:b63b5b7717ff39a530e203",
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
